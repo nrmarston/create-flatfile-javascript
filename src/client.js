23b85b80aa0e4b3580c6096fcdb7fe39bc20a7f3
Ashley Mulligan <ashleygmulligan@gmail.com>
2023-09-14 21:27:07 -0500

    fix: not needed in this file

--- src/client.js
+++ src/client.js
@@ -1,26 +1,6 @@
 import { initializeFlatfile } from "@flatfile/javascript";
 import { workbook } from "./workbook";
 import { configureFlatfileListener } from "../listeners/simple";
-const server_url = "http://localhost:8080";
-
-//open existing space in modal
-window.openExistingFlatfileSpace = () => {
-  fetch(server_url + "/space") // Make a request to the server endpoint
-    .then((response) => response.json())
-    .then((space) => {
-      const flatfileOptions = {
-        space: {
-          id: space && space.data && space.data.id,
-          accessToken: space && space.data && space.data.accessToken,
-        },
-        displayAsModal: false,
-      };
-      initializeFlatfile(flatfileOptions);
-    })
-    .catch((error) => {
-      console.error("Error retrieving space in client:", error);
-    });
-};
 
 //create a new space in modal
 window.initializeNewFlatfileSpace = ({ publishableKey, environmentId }) => {
