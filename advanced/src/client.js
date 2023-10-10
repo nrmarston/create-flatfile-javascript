3111958c937411a61ccbf44b444cc03e438ff917
Ashley Mulligan <ashleygmulligan@gmail.com>
2023-10-10 12:35:52 -0500

    feat: moving to advanced example

--- advanced/src/client.js
+++ advanced/src/client.js
@@ -1,24 +1,43 @@
 import { initializeFlatfile } from "@flatfile/javascript";
 const server_url = "http://localhost:8080";
 
+const usersTable = {
+  "user1@example.com": "us_sp_yRuaABM5",
+  "user2@example.com": null,
+  "user3@example.com": null,
+};
+
 //open existing space in modal
-window.openFlatfile = () => {
-  fetch(server_url + "/space") // Make a request to the server endpoint
-    .then((response) => response.json())
-    .then((space) => {
-      const flatfileOptions = {
-        space: {
-          id: space && space.data && space.data.id,
-          accessToken: space && space.data && space.data.accessToken,
-        },
-        sidebarConfig: {
-          showSidebar: false,
-        },
-        // Additional props...
-      };
-      initializeFlatfile(flatfileOptions);
-    })
-    .catch((error) => {
-      console.error("Error retrieving space in client:", error);
-    });
+window.openFlatfile = (userEmail) => {
+  // Check if the user exists in the usersTable.json
+
+  if (usersTable.hasOwnProperty(userEmail)) {
+    const spaceId = usersTable[userEmail];
+    // User exists in the table, proceed to get the accessToken
+    fetch(server_url + "/space")
+      .then((response) => response.json())
+      .then((space) => {
+        const accessToken = space && space.data && space.data.accessToken;
+        // Initialize Flatfile with both spaceId and accessToken
+        const flatfileOptions = {
+          space: {
+            id: spaceId,
+            accessToken: accessToken,
+          },
+          sidebarConfig: {
+            showSidebar: false,
+          },
+          // Additional props...
+        };
+        initializeFlatfile(flatfileOptions);
+      })
+      .catch((error) => {
+        console.error("Error retrieving space in client:", error);
+        // Handle error appropriately
+      });
+  } else {
+    // User not found in the table, handle this case
+    console.error("User not found in usersTable.json");
+    // You can show an error message or take appropriate action here
+  }
 };
--- src/client.js
+++ src/client.js
@@ -1,39 +1,6 @@
 import { initializeFlatfile } from "@flatfile/javascript";
 import { workbook } from "./workbook";
 import { listener } from "../listeners/simple";
-//read file
-import * as fs from "fs";
-
-// Load the user data from the JSON file
-const userTablePath = "usersTable.json";
-
-fetch(userTablePath)
-  .then((response) => response.json())
-  .then((userTable) => {
-    // Now you have access to the userTable data
-
-    const userTableData = fs.readFileSync(userTablePath, "utf8");
-    userTable = JSON.parse(userTableData);
-    console.log(userTable);
-  })
-  .catch((error) => {
-    console.error("Error loading userTable data:", error);
-  });
-
-// Function to check if a user exists and retrieve their spaceId
-function getUserSpaceId(userId) {
-  const spaceId = userTable[userId];
-  return spaceId || null;
-}
-
-// Function to add a new user record with a generated spaceId
-function addUser(userEmail) {
-  const spaceId = `generated_spaceId_${Date.now()}`;
-  userTable[userEmail] = spaceId;
-  // Save the updated userTable back to the JSON file
-  fs.writeFileSync(userTablePath, JSON.stringify(userTable, null, 2), "utf8");
-  return spaceId;
-}
 
 //create a new space in modal
 window.openFlatfile = ({ publishableKey, environmentId }) => {
